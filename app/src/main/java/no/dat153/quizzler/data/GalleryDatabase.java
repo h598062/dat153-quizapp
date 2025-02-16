9dff4f28660f903a15e6b51f8df0e5ac6ed4415c
CGeraldRojas <cri.roj1985@gmail.com>
2025-02-16 12:42:25 +0100

    Laget GalleryItemDatabase

---
+++ app/src/main/java/no/dat153/quizzler/data/GalleryDatabase.java
@@ -0,0 +1,30 @@
+package no.dat153.quizzler.data;
+
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {GalleryItem.class}, version = 1)
+public abstract class GalleryDatabase extends RoomDatabase {
+
+
+    public abstract GalleryItemDAO galleryItemDAO();
+
+    private static volatile GalleryDatabase INSTANCE;
+
+    public static GalleryDatabase getDatabase(Context context) {
+        if (INSTANCE == null) {
+            synchronized (GalleryDatabase.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                            GalleryDatabase.class, "gallery_database")
+                            .build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+}

756ee1b9d8d61f2a5a29dffc3ddfc1fe544cc7ca
Storm <sangolts@gmail.com>
2025-02-18 16:39:07 +0100

    asdasdasd

--- app/src/main/java/no/dat153/quizzler/QuizFragment.java
+++ app/src/main/java/no/dat153/quizzler/QuizFragment.java
@@ -4,15 +4,20 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
+import android.widget.Button;
 
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
-import java.util.Random;
+import java.util.HashSet;
+import java.util.Set;
 
 import no.dat153.quizzler.databinding.FragmentQuizBinding;
 import no.dat153.quizzler.entity.QuestionItem;
 import no.dat153.quizzler.viewmodel.QuizViewModel;
+
+import com.shifthackz.catppuccin.palette.legacy.R.color;
+
 
 /**
  * A simple {@link Fragment} subclass.
@@ -28,6 +33,8 @@ public class QuizFragment extends Fragment {
 
     private FragmentQuizBinding binding;
     private QuizViewModel quizViewModel;
+    private Set<QuestionItem> usedQuestionItems = new HashSet<>();
+
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -62,22 +69,25 @@ public class QuizFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
-    }
-
-    private void knappeTrykk() {
-        Toast.makeText(getContext(), "Klikket på knapp", Toast.LENGTH_SHORT).show();
+        quizViewModel = new QuizViewModel(requireActivity().getApplication());
     }
 
     private void oppdaterQuiz() {
-        // Oppdaterer quiz
-        quizViewModel = new QuizViewModel(requireActivity().getApplication());
-        quizViewModel.getQuestions().observe(getViewLifecycleOwner(), questions -> {
-            if (questions != null && !questions.isEmpty()) {
-                // Select a random image from the questions
-                Random random = new Random();
-                int randomIndex = random.nextInt(questions.size());
-                QuestionItem randomItem = questions.get(randomIndex);
-                binding.imageView.setImageURI(randomItem.getImageUri());
+        quizViewModel.getQuestions().observe(getViewLifecycleOwner(), questionItems -> {
+            if (questionItems != null) {
+                quizViewModel.settOppSvarAlternativer();
+            }
+        });
+        quizViewModel.getCorrectItem().observe(getViewLifecycleOwner(), questionItem -> {
+            if (questionItem != null) {
+                binding.imageView.setImageURI(questionItem.getImageUri());
+            }
+        });
+        quizViewModel.getQuestionItems().observe(getViewLifecycleOwner(), questionItems -> {
+            if (questionItems != null) {
+                binding.btnAnswerA.setText(questionItems.get(0).getImageText());
+                binding.btnAnswerB.setText(questionItems.get(1).getImageText());
+                binding.btnAnswerC.setText(questionItems.get(2).getImageText());
             }
         });
     }
@@ -89,12 +99,22 @@ public class QuizFragment extends Fragment {
         binding = FragmentQuizBinding.inflate(inflater, container, false);
         View view = binding.getRoot();
 
-        binding.btnAnswerA.setOnClickListener(v -> knappeTrykk());
-        binding.btnAnswerB.setOnClickListener(v -> knappeTrykk());
-        binding.btnAnswerC.setOnClickListener(v -> knappeTrykk());
+        binding.btnAnswerA.setOnClickListener(v -> checkCorrectGuess(quizViewModel.getQuestionItems().getValue().get(0), v, binding.btnAnswerB, binding.btnAnswerC));
+        binding.btnAnswerB.setOnClickListener(v -> checkCorrectGuess(quizViewModel.getQuestionItems().getValue().get(1), v, binding.btnAnswerA, binding.btnAnswerC));
+        binding.btnAnswerC.setOnClickListener(v -> checkCorrectGuess(quizViewModel.getQuestionItems().getValue().get(2), v, binding.btnAnswerA, binding.btnAnswerB));
 
         oppdaterQuiz();
 
         return view;
+    }
+
+    private void checkCorrectGuess(QuestionItem item, View v, Button otherBtn1, Button otherBtn2) {
+        if (quizViewModel.knappeTrykk(item)) {
+            v.setBackgroundColor(ContextCompat.getColor(requireContext(), color.catppuccin_mocha_green));
+            otherBtn1.setBackgroundColor(ContextCompat.getColor(requireContext(), color.catppuccin_mocha_red));
+            otherBtn2.setBackgroundColor(ContextCompat.getColor(requireContext(), color.catppuccin_mocha_red));
+        } else {
+            v.setBackgroundColor(ContextCompat.getColor(requireContext(), color.catppuccin_mocha_red));
+        }
     }
 }
--- app/src/main/java/no/dat153/quizzler/viewmodel/QuizViewModel.java
+++ app/src/main/java/no/dat153/quizzler/viewmodel/QuizViewModel.java
@@ -1,21 +1,35 @@
 package no.dat153.quizzler.viewmodel;
 
 import android.app.Application;
+import android.net.Uri;
+import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Random;
+import java.util.Set;
 
 import no.dat153.quizzler.QuizFragment;
+import no.dat153.quizzler.R;
 import no.dat153.quizzler.data.QuestionRepo;
 import no.dat153.quizzler.entity.QuestionItem;
 
 public class QuizViewModel extends ViewModel {
     private QuestionRepo questionRepo;
     private LiveData<List<QuestionItem>> questions;
+    private MutableLiveData<Integer> correctGuesses = new MutableLiveData<>(0);
+    private MutableLiveData<Integer> totalGuesses = new MutableLiveData<>(0);
+    private MutableLiveData<List<QuestionItem>> questionItems = new MutableLiveData<>();
+
+    private MutableLiveData<QuestionItem> correctItem = new MutableLiveData<>();
 
     public QuizViewModel(@NonNull Application application) {
         questionRepo = new QuestionRepo(application);
@@ -24,5 +38,59 @@ public class QuizViewModel extends ViewModel {
 
     public LiveData<List<QuestionItem>> getQuestions() {
         return questions;
+    }
+
+    public MutableLiveData<Integer> getCorrectGuesses() {
+        return correctGuesses;
+    }
+
+    public MutableLiveData<Integer> getTotalGuesses() {
+        return totalGuesses;
+    }
+
+    public void setCorrectGuesses(MutableLiveData<Integer> correctGuesses) {
+        this.correctGuesses = correctGuesses;
+    }
+
+    public void setTotalGuesses(MutableLiveData<Integer> totalGuesses) {
+        this.totalGuesses = totalGuesses;
+    }
+
+    public MutableLiveData<List<QuestionItem>> getQuestionItems() {
+        return questionItems;
+    }
+
+    public MutableLiveData<QuestionItem> getCorrectItem() {
+        return correctItem;
+    }
+
+    public boolean knappeTrykk(QuestionItem item) {
+        totalGuesses = getTotalGuesses();
+        totalGuesses.setValue(totalGuesses.getValue() + 1);
+
+        if (item.equals(correctItem)) {
+            correctGuesses = getCorrectGuesses();
+            correctGuesses.setValue(correctGuesses.getValue() + 1);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void settOppSvarAlternativer() {
+        Random random = new Random();
+        correctItem = new MutableLiveData<>(questions.getValue().get(random.nextInt(questions.getValue().size())));
+        Set<QuestionItem> svarAlternativer = new HashSet<>();
+        svarAlternativer.add(correctItem.getValue());
+
+        while (svarAlternativer.size() < 3) {
+            int tilfeldig = random.nextInt(questions.getValue().size());
+            svarAlternativer.add(questions.getValue().get(tilfeldig));
+        }
+
+        ArrayList<QuestionItem> svarListe = new ArrayList<>(svarAlternativer);
+        Collections.shuffle(svarListe);
+
+        questionItems.setValue(svarListe);
     }
 }

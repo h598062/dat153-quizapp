99bf29181e35fbc1930a524c9335bbc0873759ce
Storm <sangolts@gmail.com>
2025-02-26 14:46:59 +0100

    Test main activity funkekeke på nytt

--- app/src/androidTest/java/no/dat153/quizzler/MainActivityTest.java
+++ app/src/androidTest/java/no/dat153/quizzler/MainActivityTest.java
@@ -6,31 +6,26 @@ import static androidx.test.espresso.intent.Intents.intended;
 import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 
-import androidx.test.core.app.ActivityScenario;
-import androidx.test.espresso.intent.Intents;
+import androidx.test.espresso.intent.rule.IntentsRule;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
 
-import org.junit.After;
-import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import no.dat153.quizzler.view.GalleryActivity;
 import no.dat153.quizzler.view.MainActivity;
 import no.dat153.quizzler.view.QuizActivity;
 
+@RunWith(AndroidJUnit4.class)
 public class MainActivityTest {
-    private ActivityScenario<MainActivity> scenario;
 
-    @Before
-    public void setUp() {
-        scenario = ActivityScenario.launch(MainActivity.class);
-        Intents.init();
-    }
+    @Rule
+    public ActivityScenarioRule<MainActivity> activityScenarioRule = new ActivityScenarioRule<>(MainActivity.class);
 
-    @After
-    public void tearDown() {
-        Intents.release();
-        scenario.close();
-    }
+    @Rule
+    public IntentsRule intentsTestRule = new IntentsRule();
 
     @Test
     public void TestStartQuizButton() {
